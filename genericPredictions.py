fead99d19da7e3a2c0f7e7ff795121cda0d312b7
Mathew V Manoj <mathew.manoj1995@gmail.com>
2024-04-16 17:29:03 -0400

    adding questions

--- genericPredictions.py
+++ genericPredictions.py
@@ -1,15 +1,36 @@
 # from transformers import ViltProcessor, ViltForQuestionAnswering
 from PIL import Image
 # import torch
+import os
 
 def makeGenericPredictions(question):
     # model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
     # processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
-    # test = Image.open("iu-perfect-face-v0-2kawyvlr8y1c1.jpg")
+    inputFolder = 'scraped_images'
+    for filename in os.listdir(inputFolder):
+        if os.path.isfile(os.path.join(inputFolder, filename)):
+            print(filename)
+    test = Image.open("scraped_images/iu-perfect-face-v0-2kawyvlr8y1c1.jpg")
 
     if question == 'AGE' or question == 'age':
         text = f"What is the age of the person in the image?"
+        # result = run_inference(test, text)
+        print(text)
+    if question == 'GENDER' or question == 'gender':
+        text = f"What is the gender of the person in the image?"
+        # result = run_inference(test, text)
+        print(text)
+    if question == 'ETHNICITY' or question == 'ethnicity':
+        text = f"What is the ethnicity of the person in the image?"
+        # result = run_inference(test, text)
+        print(text)
+    if question == 'EYECOLOR' or question == 'eyeColor':
+        text = f"What is the eyecolor of the person in the image?"
+        # result = run_inference(test, text)
+        print(text)
+    if question == 'WRINKLES' or question == 'wrinkles':
+        text = f"Does the person in the image have wrinkles?"
         # result = run_inference(test, text)
         print(text)
 
